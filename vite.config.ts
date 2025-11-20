1fd43468ccaf8e6adbc095a51ec7e8987097ed7a
S Giri Shankar <jrironman196@gmail.com>
2025-11-20 10:03:19 +0000

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-    base: '/ai-analytics-page/',
-    plugins: [react()],
+export default defineConfig({
+  // your config here
 });
