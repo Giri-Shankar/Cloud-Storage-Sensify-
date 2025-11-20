5782925fd679d8a302dcc1d943d080d4acabaa6f
S Giri Shankar <jrironman196@gmail.com>
2025-11-20 15:11:52 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/ai-analytics-page/',
+    base: '/Cloud-Storage-Sensify-/',
     plugins: [react()],
 });
